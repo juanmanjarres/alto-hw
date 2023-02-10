2f8303126b8598ad8b3d40c55edd05bdafcb5381
Juan Manjarres (jcm102) <jcm102@usask.ca>
2023-02-10 01:38:25 -0600

    implemented firebase, database working, added table

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,49 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
-import {CalendarMonth} from "@mui/icons-material";
+import {
+    Button,
+    IconButton,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import {Add, CalendarMonth, Delete, Edit} from "@mui/icons-material";
+import {firestore} from "../firebase/clientApp";
+import {collection,QueryDocumentSnapshot,DocumentData,query,where,limit,getDocs} from "@firebase/firestore";
+import {useEffect, useState} from "react";
 
 
-function createData(id, seeker, giver, date, totalamt){
-  return {id, seeker, giver, date, totalamt}
+function deleteEntry(){
+
 }
 
-const rows = [
-    createData(1, 'John', 'Martha', (new Date(2020, 4, 2)).toDateString(), 1.99),
-    createData(2, 'Michael', 'Sam', (new Date(2022, 7, 2)).toDateString(), 1.99),
-    createData(3, 'Ty', 'Rob', (new Date(2019, 9, 30)).toDateString(), 1.99)
-]
+function editEntry(){
+
+}
 
 export default function Home() {
+
+    const [bookings, setBookings] = useState([])
+
+    const bookingsCollection = collection(firestore, 'bookings')
+
+    const getBookings = async () => {
+        const bookingsQuery = await getDocs(bookingsCollection)
+        const res = [];
+        bookingsQuery.forEach((doc) => {
+            res.push(doc.data())
+        })
+        setBookings(res)
+    }
+
+    useEffect(() => {
+        getBookings();
+    })
+
   return (
     <div className={styles.container}>
       <Head>
@@ -23,13 +52,17 @@ export default function Home() {
       </Head>
 
       <main>
+          <p><CalendarMonth ></CalendarMonth></p>
         <h1 className={styles.title}>
-          Bookings!
+            Bookings!
         </h1>
         <p className={styles.description}>
-          Add, edit or delete bookings <CalendarMonth></CalendarMonth>
+          Add, edit or delete bookings
         </p>
-
+          <Button variant="contained">
+              <Add sx={{ mr:1 }}></Add>   Add new booking
+          </Button>
+          <br></br>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
@@ -39,21 +72,30 @@ export default function Home() {
                             <TableCell align={"right"}>Giver</TableCell>
                             <TableCell align={"right"}>Date</TableCell>
                             <TableCell align={"right"}>Total Amount</TableCell>
+                            <TableCell align={"right"}>Actions</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {
-                            rows.map((row) => (
+                            bookings.map((bk) => (
                                 <TableRow
-                                    key={row.id}
+                                    key={bk.id}
                                     sx={{'&:last-child td, &:last-child th': { border: 0}}}>
                                     <TableCell component="th" scope="row">
-                                        {row.id}
+                                        {bk.id}
                                     </TableCell>
-                                    <TableCell align={"right"}>{row.seeker}</TableCell>
-                                    <TableCell align={"right"}>{row.giver}</TableCell>
-                                    <TableCell align={"right"}>{row.date}</TableCell>
-                                    <TableCell align={"right"}>{row.totalamt}</TableCell>
+                                    <TableCell align={"right"}>{bk.seeker}</TableCell>
+                                    <TableCell align={"right"}>{bk.giver}</TableCell>
+                                    <TableCell align={"right"}>{bk.date}</TableCell>
+                                    <TableCell align={"right"}>{bk.totalamt}</TableCell>
+                                    <TableCell align={"right"}>
+                                        <IconButton aria-label="edit" onClick={editEntry}>
+                                            <Edit />
+                                        </IconButton>
+                                        <IconButton aria-label="delete" onClick={deleteEntry}>
+                                            <Delete />
+                                        </IconButton>
+                                    </TableCell>
                                 </TableRow>
                             ))
                         }
